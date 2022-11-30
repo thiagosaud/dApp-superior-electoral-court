a25f4b1e79b2dcde3abd92d29ec64d3528918c42
THIAGO SAUD <60164558+thiagosaud@users.noreply.github.com>
2022-11-29 21:16:56 -0300

    feat: 🎸 Created Real Testnet Deploy

--- backend/src/scripts/deploy.ts
+++ backend/src/scripts/deploy.ts
@@ -1,10 +1,31 @@
+import { ethers } from "hardhat";
+
+/* eslint-disable no-console */
 async function main() {
-	//
+	const INFURA_PROVIDER = ethers.providers.InfuraProvider.getWebSocketProvider();
+	const INFURA_WALLET_OWNER = new ethers.Wallet(`${process.env.WALLET_PRIVATE_KEY}`, INFURA_PROVIDER);
+	const INFURA_WALLET_ADDRESS = await INFURA_WALLET_OWNER.getAddress();
+
+	const CONTRACT_FACTORY = await ethers.getContractFactory("Ballot");
+	const CONTRACT = await CONTRACT_FACTORY.deploy();
+
+	await CONTRACT.deployed();
+
+	const INFURA_CONTRACT_BALANCE = await INFURA_PROVIDER.getBalance(CONTRACT.address);
+
+	console.log("[INFURA:GOERLI] Contract deployed to:", CONTRACT.address);
+	console.log("[INFURA:GOERLI] Deploying contracts with account: ", INFURA_WALLET_ADDRESS);
+	console.log("[INFURA:GOERLI] Account balance: ", INFURA_CONTRACT_BALANCE);
 }
 
-main()
-	.then(() => process.exit(0))
-	.catch(() => {
-		// console.error(error);
-		process.exit(1);
-	});
+const runMain = async () => {
+	try {
+		await main();
+		process.exit(0); // exit Node process without error
+	} catch (error) {
+		console.log(error);
+		process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
+	}
+};
+
+runMain();
