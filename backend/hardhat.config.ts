adae3ff549248edb12eba70a09f469a70402ada5
THIAGO SAUD <60164558+thiagosaud@users.noreply.github.com>
2022-11-29 21:10:59 -0300

    feat: 🎸 Changed Hardhat Config

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,12 +1,25 @@
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-solhint";
+import "@nomiclabs/hardhat-etherscan";
 import "solidity-coverage";
 import "hardhat-gas-reporter";
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+/* eslint-disable no-console */
+task("accounts", "Prints the list of accounts", async ({ hre }: { hre: HardhatRuntimeEnvironment }) => {
+	const accounts = await hre.ethers.getSigners();
+	accounts.forEach(account => console.log(account.address));
+});
 
 const CONFIG: HardhatUserConfig = {
 	solidity: "0.8.17",
-	defaultNetwork: "hardhat",
+	defaultNetwork: "goerli",
 	paths: {
 		root: "./",
 		artifacts: "./artifacts",
@@ -14,9 +27,18 @@ const CONFIG: HardhatUserConfig = {
 		sources: "./src/contracts",
 		tests: "./src/test",
 	},
+	etherscan: {
+		apiKey: process.env.ETHERSCAN_PRIVATE_KEY,
+	},
 	gasReporter: {
 		enabled: true,
 		currency: "BRL",
+	},
+	networks: {
+		goerli: {
+			url: process.env.INFURA_GOERLI_URL,
+			accounts: process.env.WALLET_PRIVATE_KEY ? [process.env.WALLET_PRIVATE_KEY] : [],
+		},
 	},
 };
 
