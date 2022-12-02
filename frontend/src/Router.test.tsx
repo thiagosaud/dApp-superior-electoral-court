8fcd31e1761c165eea2d0949d4af07c59752b024
THIAGO SAUD <60164558+thiagosaud@users.noreply.github.com>
2022-12-02 20:11:21 -0300

    fix: 🐛 Correction in Unit Test [ROUTER]

--- frontend/src/Router.test.tsx
+++ frontend/src/Router.test.tsx
@@ -5,30 +5,34 @@ import FooterbarGlobal from 'components/Global/FooterbarGlobal';
 import HomePage from 'pages/HomePage';
 import Router from 'Router';
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
 describe('[ROUTER] - Testing Component', () => {
 	test('Should be have child components!', () => {
 		const renderResult = TestRenderer.create(<Router />);
+		const renderResultRendered: any = renderResult.toTree()?.rendered;
 
-		expect(renderResult.toTree()?.rendered?.type).toStrictEqual(BrowserRouter);
-		expect(renderResult.toTree()?.rendered?.props.children[0].type).toStrictEqual(AppbarGlobal);
-		expect(renderResult.toTree()?.rendered?.props.children[1].type).toStrictEqual(Routes);
-		expect(renderResult.toTree()?.rendered?.props.children[2].type).toStrictEqual(FooterbarGlobal);
+		expect(renderResultRendered.type).toStrictEqual(BrowserRouter);
+		expect(renderResultRendered.props?.children[0]?.type).toStrictEqual(AppbarGlobal);
+		expect(renderResultRendered.props?.children[1]?.type).toStrictEqual(Routes);
+		expect(renderResultRendered.props?.children[2]?.type).toStrictEqual(FooterbarGlobal);
 	});
 
 	test('Should be root route working!', () => {
 		const renderResult = TestRenderer.create(<Router />);
-		const routerProps = renderResult.toTree()?.rendered?.props.children[1].props.children[0].props;
+		const renderResultRendered: any = renderResult.toTree()?.rendered;
+		const routerProps = renderResultRendered?.props.children[1].props.children[0].props;
 
-		expect(routerProps.path).toStrictEqual('/');
-		expect(routerProps.element.type).toStrictEqual(HomePage);
-		expect(routerProps.index).toBe(true);
+		expect(routerProps?.path).toStrictEqual('/');
+		expect(routerProps?.element?.type).toStrictEqual(HomePage);
+		expect(routerProps?.index).toBe(true);
 	});
 
 	test('Should be 404 route working!', () => {
 		const renderResult = TestRenderer.create(<Router />);
-		const routerProps = renderResult.toTree()?.rendered?.props.children[1].props.children[1].props;
+		const renderResultRendered: any = renderResult.toTree()?.rendered;
+		const routerProps = renderResultRendered?.props?.children[1]?.props?.children[1]?.props;
 
-		expect(routerProps.path).toStrictEqual('*');
-		expect(routerProps.element.type).toStrictEqual(Navigate);
+		expect(routerProps?.path).toStrictEqual('*');
+		expect(routerProps?.element?.type).toStrictEqual(Navigate);
 	});
 });
