0cd8293787abc3e2e4e792c71ea2284a129ec797
THIAGO SAUD <60164558+thiagosaud@users.noreply.github.com>
2022-12-17 11:01:41 -0300

    feat: 🎸 Refactoring [HOME PAG]

--- frontend/src/pages/HomePage/index.tsx
+++ frontend/src/pages/HomePage/index.tsx
@@ -1,100 +1,76 @@
-import { memo } from 'react';
+import { memo, useCallback, useEffect, useMemo, useReducer, useState } from 'react';
 import { Button } from 'react-bootstrap';
-import CandidateList from 'components/List/CandidateList';
-import VotingStatisticList from 'components/List/VotingStatisticList';
+import { chain } from 'mathjs';
 import PageTemplate from 'templates/TemplatePage';
+import VotingStatisticList from 'components/Lists/VotingStatisticList';
+import CandidateList from 'components/Lists/CandidateList';
+import { useSolidityContractProvider } from 'providers/useSolidityContractProvider';
+import { TypeInfuraStorageData, TypeInfuraData } from 'hooks/useStorageDB';
+import CandidateListSkeleton from 'components/Skeletons/CandidateListSkeleton';
 
 function HomePage() {
+	const { actions, states } = useSolidityContractProvider();
+	const [isLoadingElectoralResult, updateIsLoadingElectoralResult] = useReducer(state => !state, true);
+	const [electoralResult, setElectoralResult] = useState<TypeInfuraStorageData>(null);
+
+	const votes = useMemo(() => {
+		const TOTAL_CONFIRMED_VOTES = electoralResult?.totalConfirmedVotes || 0;
+		const TOTAL_ABSTENTION_VOTES = electoralResult?.abstentionVotes.vote.total || 0;
+		const TOTAL_VOTES = chain([TOTAL_CONFIRMED_VOTES, TOTAL_ABSTENTION_VOTES]).sum().done();
+
+		return {
+			confirmed: {
+				total: TOTAL_CONFIRMED_VOTES,
+				totalPercentage: chain(TOTAL_CONFIRMED_VOTES).divide(TOTAL_VOTES).multiply(100),
+			},
+			abstention: {
+				total: TOTAL_ABSTENTION_VOTES,
+				totalPercentage: chain(TOTAL_ABSTENTION_VOTES).divide(TOTAL_VOTES).multiply(100),
+			},
+		};
+	}, [electoralResult]);
+
+	const getCandidateList = useCallback(
+		({ confirmedVotes, totalConfirmedVotes }: TypeInfuraData) =>
+			confirmedVotes.map(({ candidate, vote }) => ({
+				// eslint-disable-next-line no-console
+				onConfirmVote: (number: number) => console.log(number),
+				number: candidate,
+				hasVoted: false,
+				votesConfirmed: {
+					total: vote.total,
+					totalPercentage: chain(vote.total).divide(totalConfirmedVotes).multiply(100),
+				},
+			})),
+		[]
+	);
+
+	const cacheData = useCallback(() => {
+		if (!states.isLoadingDB && states.infuraDB) {
+			setElectoralResult(states.infuraDB);
+		}
+
+		if (!states.isLoadingDB && !states.infuraDB) {
+			actions.getElectoralResult().then(data => setElectoralResult(data));
+		}
+
+		updateIsLoadingElectoralResult();
+	}, [actions, states]);
+
+	useEffect(() => cacheData(), [cacheData]);
+
 	return (
 		<PageTemplate>
-			<VotingStatisticList
-				votes={{
-					confirmed: {
-						total: 124252799,
-						totalPercentage: 99.99,
-					},
-					abstention: {
-						total: 124252799,
-						totalPercentage: 89.85,
-					},
-				}}
-			/>
+			<VotingStatisticList isLoading={isLoadingElectoralResult} votes={votes} />
 
 			<div className='mt-4 mb-3 d-flex justify-content-between align-items-center'>
 				<h5 className='mb-0'>President</h5>
 				<Button variant='light'>Abstain from Voting</Button>
 			</div>
 
-			<CandidateList
-				data={[
-					{
-						// eslint-disable-next-line no-console
-						onConfirmVote: number => console.log(number),
-						number: 1,
-						avatar: 'https://raw.githubusercontent.com/thiagosaud/dApp-superior-electoral-court/main/temp/imgs/candidate-1.png',
-						hasVoted: true,
-						votesConfirmed: {
-							total: 124252799,
-							totalPercentage: 99.99,
-						},
-					},
-					{
-						// eslint-disable-next-line no-console
-						onConfirmVote: number => console.log(number),
-						number: 2,
-						avatar: 'https://raw.githubusercontent.com/thiagosaud/dApp-superior-electoral-court/main/temp/imgs/candidate-2.png',
-						hasVoted: false,
-						votesConfirmed: {
-							total: 124252796,
-							totalPercentage: 99.59,
-						},
-					},
-					{
-						// eslint-disable-next-line no-console
-						onConfirmVote: number => console.log(number),
-						number: 3,
-						avatar: 'https://raw.githubusercontent.com/thiagosaud/dApp-superior-electoral-court/main/temp/imgs/candidate-3.png',
-						hasVoted: false,
-						votesConfirmed: {
-							total: 124252796,
-							totalPercentage: 99.59,
-						},
-					},
-					{
-						// eslint-disable-next-line no-console
-						onConfirmVote: number => console.log(number),
-						number: 4,
-						avatar: 'https://raw.githubusercontent.com/thiagosaud/dApp-superior-electoral-court/main/temp/imgs/candidate-4.png',
-						hasVoted: false,
-						votesConfirmed: {
-							total: 124252796,
-							totalPercentage: 99.59,
-						},
-					},
-					{
-						// eslint-disable-next-line no-console
-						onConfirmVote: number => console.log(number),
-						number: 5,
-						avatar: 'https://raw.githubusercontent.com/thiagosaud/dApp-superior-electoral-court/main/temp/imgs/candidate-5.png',
-						hasVoted: false,
-						votesConfirmed: {
-							total: 124252796,
-							totalPercentage: 99.59,
-						},
-					},
-					{
-						// eslint-disable-next-line no-console
-						onConfirmVote: number => console.log(number),
-						number: 6,
-						avatar: 'https://raw.githubusercontent.com/thiagosaud/dApp-superior-electoral-court/main/temp/imgs/candidate-6.png',
-						hasVoted: false,
-						votesConfirmed: {
-							total: 124252796,
-							totalPercentage: 99.59,
-						},
-					},
-				]}
-			/>
+			{(states.isLoadingDB || isLoadingElectoralResult) && !electoralResult && <CandidateListSkeleton />}
+
+			{!states.isLoadingDB && !isLoadingElectoralResult && electoralResult && <CandidateList data={getCandidateList(electoralResult)} />}
 		</PageTemplate>
 	);
 }
