31af220eb32d6a3a86301ecadb5ae6e5a0d1caa9
THIAGO SAUD <60164558+thiagosaud@users.noreply.github.com>
2022-12-17 01:39:42 -0300

    feat: 🎸 Added isRounded Property [GENERIC_SKELETON]

--- frontend/src/components/Skeletons/GenericSkeleton/index.tsx
+++ frontend/src/components/Skeletons/GenericSkeleton/index.tsx
@@ -3,10 +3,15 @@ import { Button } from 'react-bootstrap';
 interface IProps {
 	height: string;
 	width: string;
+	isRounded?: boolean;
 }
 
-function GenericSkeleton({ height, width }: IProps) {
-	return <Button variant='warning' style={{ height, width }} disabled />;
+function GenericSkeleton({ height, width, isRounded }: IProps) {
+	return <Button variant='warning' style={{ height, width, borderRadius: isRounded ? '100%' : 'auto' }} disabled />;
 }
+
+GenericSkeleton.defaultProps = {
+	isRounded: false,
+};
 
 export default GenericSkeleton;
