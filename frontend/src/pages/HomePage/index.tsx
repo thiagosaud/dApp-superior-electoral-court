9dd126b2447a913865a766d7cb872595b77ecd32
THIAGO SAUD <60164558+thiagosaud@users.noreply.github.com>
2022-12-17 13:02:43 -0300

    feat: 🎸 Added Loading Skeleton [HOME PAGE]

--- frontend/src/pages/HomePage/index.tsx
+++ frontend/src/pages/HomePage/index.tsx
@@ -4,9 +4,10 @@ import { chain } from 'mathjs';
 import PageTemplate from 'templates/TemplatePage';
 import VotingStatisticList from 'components/Lists/VotingStatisticList';
 import CandidateList from 'components/Lists/CandidateList';
+import CandidateListSkeleton from 'components/Skeletons/CandidateListSkeleton';
+import GenericSkeleton from 'components/Skeletons/GenericSkeleton';
 import { useSolidityContractProvider } from 'providers/useSolidityContractProvider';
 import { TypeInfuraStorageData, TypeInfuraData } from 'hooks/useStorageDB';
-import CandidateListSkeleton from 'components/Skeletons/CandidateListSkeleton';
 
 function HomePage() {
 	const { actions, states } = useSolidityContractProvider();
@@ -30,13 +31,36 @@ function HomePage() {
 		};
 	}, [electoralResult]);
 
+	const hasVoted = useMemo(() => {
+		if (electoralResult) {
+			const { abstentionVotes, confirmedVotes } = electoralResult;
+			const ELECTOR_ABSTENTION_VOTE = abstentionVotes.elector.find(wallet => wallet === states.metamaskDB);
+			const ELECTOR_CONFIRMED_VOTE = confirmedVotes.find(({ elector }) => elector.find(wallet => wallet === states.metamaskDB));
+
+			return !!(ELECTOR_ABSTENTION_VOTE || ELECTOR_CONFIRMED_VOTE);
+		}
+
+		return false;
+	}, [electoralResult, states]);
+
+	const showSkeleton = useMemo(() => {
+		if ((states.isLoadingDB || isLoadingElectoralResult) && !electoralResult) {
+			return true;
+		}
+
+		if (!states.isLoadingDB && !isLoadingElectoralResult && electoralResult) {
+			return false;
+		}
+
+		return true;
+	}, [states, isLoadingElectoralResult, electoralResult]);
+
 	const getCandidateList = useCallback(
 		({ confirmedVotes, totalConfirmedVotes }: TypeInfuraData) =>
 			confirmedVotes.map(({ candidate, vote }) => ({
 				// eslint-disable-next-line no-console
 				onConfirmVote: (number: number) => console.log(number),
 				number: candidate,
-				hasVoted: false,
 				votesConfirmed: {
 					total: vote.total,
 					totalPercentage: chain(vote.total).divide(totalConfirmedVotes).multiply(100),
@@ -48,13 +72,15 @@ function HomePage() {
 	const cacheData = useCallback(() => {
 		if (!states.isLoadingDB && states.infuraDB) {
 			setElectoralResult(states.infuraDB);
+			updateIsLoadingElectoralResult();
 		}
 
 		if (!states.isLoadingDB && !states.infuraDB) {
-			actions.getElectoralResult().then(data => setElectoralResult(data));
+			actions.getElectoralResult().then(data => {
+				setElectoralResult(data);
+				updateIsLoadingElectoralResult();
+			});
 		}
-
-		updateIsLoadingElectoralResult();
 	}, [actions, states]);
 
 	useEffect(() => cacheData(), [cacheData]);
@@ -65,12 +91,18 @@ function HomePage() {
 
 			<div className='mt-4 mb-3 d-flex justify-content-between align-items-center'>
 				<h5 className='mb-0'>President</h5>
-				<Button variant='light'>Abstain from Voting</Button>
+
+				{showSkeleton ? (
+					<GenericSkeleton height='38px' width='172px' />
+				) : (
+					<Button variant='light' disabled={hasVoted}>
+						Abstain from Voting
+					</Button>
+				)}
 			</div>
 
-			{(states.isLoadingDB || isLoadingElectoralResult) && !electoralResult && <CandidateListSkeleton />}
-
-			{!states.isLoadingDB && !isLoadingElectoralResult && electoralResult && <CandidateList data={getCandidateList(electoralResult)} />}
+			{showSkeleton && !electoralResult && <CandidateListSkeleton />}
+			{electoralResult && <CandidateList data={getCandidateList(electoralResult)} hasVoted={hasVoted} />}
 		</PageTemplate>
 	);
 }
