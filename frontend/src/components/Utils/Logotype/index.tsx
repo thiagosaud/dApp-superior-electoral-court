0bacafc19f381931f273bcf13e771ec09ed74b02
THIAGO SAUD <60164558+thiagosaud@users.noreply.github.com>
2022-12-18 02:36:35 -0300

    feat: 🎸 Added new Size and Changed Loading Type [LOGOTYPE]

--- frontend/src/components/Utils/Logotype/index.tsx
+++ frontend/src/components/Utils/Logotype/index.tsx
@@ -2,15 +2,16 @@ import ImageSRC from 'assets/images/tse-logotype.png';
 import styled from 'styled-components';
 
 interface IProps {
-	size: 'xs';
+	size: 'xs' | 'md';
 }
 
 const Image = styled.img.attrs({
 	src: ImageSRC,
 	alt: 'Superior Electoral Court',
-	loading: 'lazy',
+	loading: 'eager',
 })<IProps>`
 	${({ size }) => size === 'xs' && { width: '35px', height: '35px' }}
+	${({ size }) => size === 'md' && { width: '120px', height: '120px' }}
 `;
 
 export default function Logotype({ size }: IProps) {
