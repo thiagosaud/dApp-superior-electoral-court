d8a60637c78dc44fd023e5776db55033998ffba1
THIAGO SAUD <60164558+thiagosaud@users.noreply.github.com>
2022-12-18 02:35:26 -0300

    feat: 🎸 Created Component [LOADING]

---
+++ frontend/src/components/Utils/Loading/index.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import ProgressAnimation from 'animations/ProgressAnimation';
+
+const Wrapper = styled.div`
+	position: relative;
+	width: 130px;
+	height: 2px;
+	margin-top: 22px;
+	overflow: hidden;
+	background: var(--bs-gray-500);
+
+	::before {
+		${ProgressAnimation}
+		position: absolute;
+		left: -34px;
+		width: 68px;
+		height: 2px;
+		content: '';
+		background: var(--bs-warning);
+	}
+`;
+
+function Loading() {
+	return <Wrapper />;
+}
+
+export default Loading;
