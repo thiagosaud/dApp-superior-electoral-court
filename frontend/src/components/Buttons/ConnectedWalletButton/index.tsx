1db6f82b8121055ac0fa3d3f37f19778bce6ec3e
THIAGO SAUD <60164558+thiagosaud@users.noreply.github.com>
2022-12-21 01:20:09 -0300

    feat: 🎸 Created Logs Item and Added Icon [CONNECTED_WALLET]

--- frontend/src/components/Buttons/ConnectedWalletButton/index.tsx
+++ frontend/src/components/Buttons/ConnectedWalletButton/index.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from 'react';
 import { Dropdown, DropdownButton } from 'react-bootstrap';
+import { RxExit } from 'react-icons/rx';
 
 interface IProps {
 	onLogout: () => void;
@@ -10,10 +11,17 @@ interface IProps {
 function ConnectedWalletButton({ onLogout, wallet, isDisabled }: IProps) {
 	const hash = useMemo(() => wallet.substring(0, 11), [wallet]);
 
+	const openContractPage = () =>
+		window.open(`https://goerli.etherscan.io/address/${process.env.REACT_APP_SOLIDITY_CONTRACT_ADDRESS}`, '_blank', 'noopener,noreferrer');
+
 	return (
 		<DropdownButton variant='warning' align='end' title={hash} disabled={isDisabled}>
-			<Dropdown.Item as='button' onClick={onLogout}>
+			<Dropdown.Item as='button' onClick={openContractPage}>
+				Logs
+			</Dropdown.Item>
+			<Dropdown.Item as='button' onClick={onLogout} className='d-flex gap-2 align-items-center'>
 				Logout
+				<RxExit size='20' color='black' />
 			</Dropdown.Item>
 		</DropdownButton>
 	);
