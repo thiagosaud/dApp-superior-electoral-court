7d24034388c6d733874db11ca71d39e84687aa1b
THIAGO SAUD <60164558+thiagosaud@users.noreply.github.com>
2022-12-21 01:36:34 -0300

    feat: 🎸 Added Help Item and Route Logic [CONENCTED_WALLET_BTN]

--- frontend/src/components/Buttons/ConnectedWalletButton/index.tsx
+++ frontend/src/components/Buttons/ConnectedWalletButton/index.tsx
@@ -1,4 +1,5 @@
 import { useMemo } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Dropdown, DropdownButton } from 'react-bootstrap';
 import { RxExit } from 'react-icons/rx';
 
@@ -9,15 +10,22 @@ interface IProps {
 }
 
 function ConnectedWalletButton({ onLogout, wallet, isDisabled }: IProps) {
+	const navigateHook = useNavigate();
+
 	const hash = useMemo(() => wallet.substring(0, 11), [wallet]);
 
 	const openContractPage = () =>
 		window.open(`https://goerli.etherscan.io/address/${process.env.REACT_APP_SOLIDITY_CONTRACT_ADDRESS}`, '_blank', 'noopener,noreferrer');
 
+	const goToHelpPage = () => navigateHook('/help');
+
 	return (
 		<DropdownButton variant='warning' align='end' title={hash} disabled={isDisabled}>
 			<Dropdown.Item as='button' onClick={openContractPage}>
 				Logs
+			</Dropdown.Item>
+			<Dropdown.Item as='button' onClick={goToHelpPage}>
+				Help
 			</Dropdown.Item>
 			<Dropdown.Item as='button' onClick={onLogout} className='d-flex gap-2 align-items-center'>
 				Logout
